37af5da7898fca4a653f7dd5b6d041102ffaddf0
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-04-15 16:33:31 +0900

    Add ABC093-{B,C}

---
+++ ABC093/b/src/main.rs
@@ -0,0 +1,80 @@
+#![allow(unused)]
+
+#[allow(unused_imports)]
+use std::{cmp, collections, fmt, io, iter, ops, str};
+use cmp::Ordering::*;
+use collections::{HashMap, HashSet};
+use io::{Read, Write};
+
+#[cfg(feature = "debug")]
+macro_rules! debug {
+    ($fmt: expr) => { eprintln!($fmt) };
+    ($fmt: expr, $($arg: tt)*) => { eprintln!($fmt, $($arg)*) };
+}
+
+#[cfg(not(feature = "debug"))]
+macro_rules! debug {
+    ($fmt: expr) => {};
+    ($fmt: expr, $($arg: tt)*) => {};
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_owned()
+}
+
+fn read_word() -> String {
+    const CAPACITY: usize = 16;
+
+    let mut stdin = io::stdin();
+    let mut byte: [u8; 1] = [0];
+
+    loop {
+        let mut buf = Vec::with_capacity(CAPACITY);
+
+        loop {
+            let res = stdin.read(&mut byte);
+
+            if res.unwrap_or(0) == 0 || byte[0] <= b' ' {
+                break;
+            } else {
+                buf.push(byte[0]);
+            }
+        }
+
+        if buf.len() > 0 {
+            return String::from_utf8(buf).unwrap();
+        }
+    }
+}
+
+fn parse_line<T: str::FromStr>() -> T {
+    read_line().parse().ok().unwrap()
+}
+
+fn parse_word<T: str::FromStr>() -> T {
+    read_word().parse().ok().unwrap()
+}
+
+fn parse_vec<T: str::FromStr>() -> Vec<T> {
+    read_line()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn flush() {
+    io::stdout().flush();
+}
+
+fn main() {
+    let (a, b, k) = {
+        let abk = parse_vec::<i32>();
+        (abk[0], abk[1], abk[2])
+    };
+
+    for i in (a..(b + 1)).filter(|x| x - a < k || b - x < k) {
+        println!("{}", i);
+    }
+}
