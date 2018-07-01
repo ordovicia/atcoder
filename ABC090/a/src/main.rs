35873ae5fb09db17cd2643afd88d7b8b4e5b963b
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-07-01 22:53:01 +0900

    Add ABC090-A, B

---
+++ ABC090/a/src/main.rs
@@ -0,0 +1,101 @@
+#![allow(unused)]
+
+use cmp::Ordering::{Equal, Greater, Less};
+use collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
+use io::{Read, Write};
+use std::{cmp, collections, fmt, io, iter, ops, str};
+
+#[cfg(feature = "debug")]
+macro_rules! debug {
+    ($fmt: expr) => { eprintln!($fmt) };
+    ($fmt: expr, $($args: tt)*) => { eprintln!($fmt, $($args)*) };
+}
+
+#[cfg(not(feature = "debug"))]
+macro_rules! debug {
+    ($fmt:expr) => {};
+    ($fmt:expr, $($args:tt)*) => {};
+}
+
+macro_rules! vmax {
+    ($x: expr, $y: expr) => { cmp::max($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::max($x, vmax!($($args),*)) };
+}
+
+macro_rules! vmin {
+    ($x: expr, $y: expr) => { cmp::min($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::min($x, vmin!($($args),*)) };
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_owned()
+}
+
+fn read_word() -> String {
+    const CAPACITY: usize = 16;
+
+    let mut stdin = io::stdin();
+    let mut byte: [u8; 1] = [0];
+
+    loop {
+        let mut buf = Vec::with_capacity(CAPACITY);
+
+        loop {
+            let res = stdin.read(&mut byte);
+
+            if res.unwrap_or(0) == 0 || byte[0] <= b' ' {
+                break;
+            } else {
+                buf.push(byte[0]);
+            }
+        }
+
+        if buf.len() > 0 {
+            return String::from_utf8(buf).unwrap();
+        }
+    }
+}
+
+fn parse_line<T: str::FromStr>() -> T {
+    read_line().parse().ok().unwrap()
+}
+
+fn parse_word<T: str::FromStr>() -> T {
+    read_word().parse().ok().unwrap()
+}
+
+fn parse_vec<T: str::FromStr>() -> Vec<T> {
+    read_line()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn flush() {
+    io::stdout().flush();
+}
+
+fn main() {
+    let c1 = read_line();
+    let c2 = read_line();
+    let c3 = read_line();
+
+    println!(
+        "{}{}{}",
+        c1.chars().nth(0).unwrap(),
+        c2.chars().nth(1).unwrap(),
+        c3.chars().nth(2).unwrap()
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        //
+    }
+}
---
+++ ABC090/b/src/main.rs
@@ -0,0 +1,105 @@
+#![allow(unused)]
+
+use std::{cmp, collections, fmt, io, iter, ops, str};
+use cmp::Ordering::{Equal, Greater, Less};
+use collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
+use io::{Read, Write};
+
+#[cfg(feature = "debug")]
+macro_rules! debug {
+    ($fmt: expr) => { eprintln!($fmt) };
+    ($fmt: expr, $($args: tt)*) => { eprintln!($fmt, $($args)*) };
+}
+
+#[cfg(not(feature = "debug"))]
+macro_rules! debug {
+    ($fmt: expr) => {};
+    ($fmt: expr, $($args: tt)*) => {};
+}
+
+macro_rules! vmax {
+    ($x: expr, $y: expr) => { cmp::max($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::max($x, vmax!($($args),*)) };
+}
+
+macro_rules! vmin {
+    ($x: expr, $y: expr) => { cmp::min($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::min($x, vmin!($($args),*)) };
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_owned()
+}
+
+fn read_word() -> String {
+    const CAPACITY: usize = 16;
+
+    let mut stdin = io::stdin();
+    let mut byte: [u8; 1] = [0];
+
+    loop {
+        let mut buf = Vec::with_capacity(CAPACITY);
+
+        loop {
+            let res = stdin.read(&mut byte);
+
+            if res.unwrap_or(0) == 0 || byte[0] <= b' ' {
+                break;
+            } else {
+                buf.push(byte[0]);
+            }
+        }
+
+        if buf.len() > 0 {
+            return String::from_utf8(buf).unwrap();
+        }
+    }
+}
+
+fn parse_line<T: str::FromStr>() -> T {
+    read_line().parse().ok().unwrap()
+}
+
+fn parse_word<T: str::FromStr>() -> T {
+    read_word().parse().ok().unwrap()
+}
+
+fn parse_vec<T: str::FromStr>() -> Vec<T> {
+    read_line()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn flush() {
+    io::stdout().flush();
+}
+
+fn main() {
+    let min = parse_word::<i32>();
+    let max = parse_word::<i32>();
+
+    let count = (min..(max + 1)).filter(|x| is_palindrome(*x)).count();
+    println!("{}", count);
+}
+
+fn is_palindrome(x: i32) -> bool {
+    let chars = {
+        let s = x.to_string();
+        s.chars().collect::<Vec<_>>()
+    };
+
+    chars[0] == chars[4] && chars[1] == chars[3]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        //
+    }
+}
