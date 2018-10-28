29c91cd5e2189ffbc3db09f708a001fc0cae6090
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-10-28 12:09:00 +0900

    Add Tenka1PBC-A, B, C

---
+++ Tenka1PBC/b/src/main.rs
@@ -0,0 +1,127 @@
+#![allow(unused)]
+
+use std::{cell, cmp, collections, fmt, ops};
+
+use cell::{Cell, RefCell};
+use cmp::Ordering::{Equal, Greater, Less};
+use collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
+use fmt::{Debug, Display};
+
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[cfg(feature = "debug")]
+macro_rules! dbg {
+    ($($args: tt)*) => { eprint!($($args)*) };
+}
+
+#[cfg(feature = "debug")]
+macro_rules! dbgln {
+    ($($args: tt)*) => { eprintln!($($args)*) };
+}
+
+#[cfg(not(feature = "debug"))]
+macro_rules! dbg {
+    ($($args:tt)*) => {};
+}
+
+#[cfg(not(feature = "debug"))]
+macro_rules! dbgln {
+    ($($args:tt)*) => {};
+}
+
+mod num {
+    //
+}
+
+macro_rules! vmin {
+    ($x: expr, $y: expr) => { cmp::min($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::min($x, vmin!($($args),*)) };
+}
+
+macro_rules! vmax {
+    ($x: expr, $y: expr) => { cmp::max($x, $y) };
+    ($x: expr, $($args: expr),*) => { cmp::max($x, vmax!($($args),*)) };
+}
+
+mod bits {
+    pub fn is_high(bits: u64, pos: u64) -> bool {
+        bits & (1 << pos) != 0
+    }
+}
+
+fn main() {
+    input!(a: i32, b: i32, k: i32);
+
+    let mut a = a;
+    let mut b = b;
+
+    for i in 0..k {
+        if i % 2 == 0 {
+            b += a / 2;
+            a /= 2;
+        } else {
+            a += b / 2;
+            b /= 2;
+        }
+    }
+
+    println!("{} {}", a, b);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        //
+    }
+}
