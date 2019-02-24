26a0a0e00bc4fda712737d1312df98b766959375
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-02-25 00:13:55 +0900

    Add ABC119 A, B, C, D

---
+++ ABC119/b/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function error() {
+    echo -e "\033[031m" $1 "\033[0m"
+}
+
+root=$(cd $(dirname $0); pwd)
+cd $root
+
+echo -n "Building ..."
+if ! cargo build 2> /dev/null; then
+    error "Failed"
+    exit 1
+else
+    echo
+fi
+
+cd $root/testcases
+
+for f in *.in; do
+    if [ $(wc -l $f | cut -d' ' -f 1) -eq 0 ]; then
+        continue;
+    fi
+
+    echo -n "Running against \"$f\" ... "
+
+    expected="$(cat ${f%.in}.out)"
+    actually="$(cargo run < $f 2> /dev/null)"
+
+    if [ "$expected" != "$actually" ]; then
+        error "Failed."
+        cat << EOF
+Expected:
+$expected
+
+Actually:
+$actually
+
+EOF
+    else
+        echo
+    fi
+done
